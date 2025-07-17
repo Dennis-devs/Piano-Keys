bbda5174ec9484c68d17a95483412cd3c271895a
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-07-17 17:48:24 +0300

    Improving Musical Keyboard buttons

--- projectsMe/drumKeys/script.js
+++ projectsMe/drumKeys/script.js
@@ -2,11 +2,18 @@ const keys = document.querySelectorAll('.keys');
 //console.log(keys)
 keys.forEach(item => item.style.cssText = 'color: violet; background: ivory; padding: 50px; border: solid; border-color: gold; font-size: 40px; border-bottom-right-radius: 15%; border-bottom-left-radius: 15%');
 
-function piano(e){
+function makeSound(e){
 const audio = document.querySelector(`audio[data-keys='${e.keyCode}']`);
 if(!audio) return;
 audio.currentTime = 0;
 audio.play();
+
+const onSound = document.querySelector(`section[data-keys='${e.keyCode}']`);
+onSound.style.backgroundColor = 'gray'
+}
+function stopSound(e){
+    const offSound = document.querySelector(`section[data-keys='${e.keyCode}']`);
+    offSound.style.backgroundColor = 'key' ? '' : ''
 }
 
 const player = document.querySelector('.music');
@@ -26,4 +33,5 @@ function classical2(){
 
 player.addEventListener('click', classical);
 player2.addEventListener('click', classical2);
-window.addEventListener('keydown', piano);
+window.addEventListener('keydown', makeSound);
+window.addEventListener('keyup', stopSound);
